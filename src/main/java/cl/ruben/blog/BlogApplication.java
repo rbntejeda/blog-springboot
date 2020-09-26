77dcc0d003f1be89bb07082b37a730662edfe137
Ruben Eduardo Tejeda Roa <rubentejedaroa@gmail.com>
2020-09-26 01:15:40 -0300

    init hello

---
+++ src/main/java/cl/ruben/blog/BlogApplication.java
@@ -0,0 +1,21 @@
+package cl.ruben.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s", name);
+	}
+}
